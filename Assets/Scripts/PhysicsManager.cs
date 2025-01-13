0af6afa98a1f5af6390238ec3b2f36bbefb44bfb
Louis YANNE <louis.yanne@intra.gobelins.fr>
2025-01-13 18:46:25 +0100

    adaptation physicsManager events

--- Assets/Scripts/PhysicsManager.cs
+++ Assets/Scripts/PhysicsManager.cs
@@ -2,11 +2,19 @@ using NUnit.Framework;
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class PhysicsManager : MonoBehaviour
 {
     // Design pattern du singleton
     private static PhysicsManager _instance; // instance statique du game state manager
+
+    //Gestion des phases
+    private bool dispersionPhase = false;
+    public float minSpeedForBalls {  get; private set; }
+
+    //Array des billes restantes
+    List<BallRoll> RemainingBalls = new List<BallRoll>();
 
     public static PhysicsManager Instance
     {
@@ -15,6 +23,7 @@ public class PhysicsManager : MonoBehaviour
             return _instance;
         }
     }
+
 
     private void Awake()
     {
@@ -29,11 +38,51 @@ public class PhysicsManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        BallRoll[] ballRolls = FindObjectsByType<BallRoll>(FindObjectsSortMode.None);
+        RemainingBalls = ballRolls.ToList();
+    }
+
+    private void Update()
+    {
+        // Vérification qu'on est en phase de dispersion
+        if (dispersionPhase) {
+            // Ceci revient à vérifier que toutes les billes sont arretees
+            bool BallsAllMotionless = true;
+            foreach (BallRoll ball in RemainingBalls) 
+            {
+                // ON controle la vitesse de toutes les billes existantes
+                if(ball.speed > minSpeedForBalls)
+                {
+                    //Des qu'on trouve une bille qui n'est pas arretee, on peut arreter la vérification ici
+                    BallsAllMotionless = false;
+                    break;
+                }
+            }
+            // Si toutes les billes sont arretees, on leve l'evenement signalant la fin de la phase de dispersion
+            if (BallsAllMotionless) {
+                dispersionPhase = false;
+
+                // collisions info : fastest ball id, slowest ball id, positive or not
+                List<Tuple<int, int, bool>> collisions = new List<Tuple<int, int, bool>>();
+                // pocketings info : ball id, pocket id
+                List<Tuple<int, int>> pocketings = new List<Tuple<int, int>>();
+
+                Debug.Log("Physics Manager: Requesting rules application.");
+                EventBus.Publish(new EventApplyRulesRequest(collisions, pocketings));
+            }
+        }
+        
+    }
+
     private void OnEnable()
     {
         // subscribe to all events that this component needs to listen to at all time
         EventBus.Subscribe<EventInitialBallsSetupRequest>(HandleBallsInitialSetupRequest);
         EventBus.Subscribe<EventApplyForceToWhiteRequest>(HandleForceApplicationToWhiteRequest);
+        // abonnement à l'evenement empochement de billes
+        BallRoll.BallPocketed += UnregisterBall;
     }
 
     private void OnDisable()
@@ -41,6 +90,8 @@ public class PhysicsManager : MonoBehaviour
         // Unsubscribe from all events before getting destroyed to avoid memory leaks
         EventBus.Unsubscribe<EventInitialBallsSetupRequest>(HandleBallsInitialSetupRequest);
         EventBus.Unsubscribe<EventApplyForceToWhiteRequest>(HandleForceApplicationToWhiteRequest);
+        // abonnement à l'evenement empochement de billes
+        BallRoll.BallPocketed += UnregisterBall;
     }
 
     private void OnDestroy()
@@ -68,6 +119,7 @@ public class PhysicsManager : MonoBehaviour
     /// <param name="requestEvent">Contains _angle and _force</param>
     private void HandleForceApplicationToWhiteRequest(EventApplyForceToWhiteRequest requestEvent)
     {
+        dispersionPhase = true;
         Debug.Log("Physics Manager: Applying force to white ball.");
         // TODO:
         // - Apply force
@@ -75,14 +127,11 @@ public class PhysicsManager : MonoBehaviour
         // - Record collisions (balls and positive/negative status)
         // - Record pocketings
         // - Record new positions of balls
+    }
 
-        // collisions info : fastest ball id, slowest ball id, positive or not
-        List<Tuple<int, int, bool>> collisions = new List<Tuple<int, int, bool>>();
-        // pocketings info : ball id, pocket id
-        List<Tuple<int, int>> pocketings = new List<Tuple<int, int>>();
-
-        Debug.Log("Physics Manager: Requesting rules application.");
-        EventBus.Publish(new EventApplyRulesRequest(collisions, pocketings));
+    private void UnregisterBall(BallRoll ball)
+    {
+        RemainingBalls.Remove(ball);
     }
 
 }
